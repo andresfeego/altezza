b761878ac5f53bca493fb3da641b01af4ef5ace5
Andres Manrique <andres.feego@gmail.com>
2026-05-03 00:47:49 -0500

    Agrega modulo adults only notice y ajusta overlays classic

--- components/invitaciones-publicas/templates/wedding-terracota/index.js
+++ components/invitaciones-publicas/templates/wedding-terracota/index.js
@@ -8,6 +8,7 @@ import CountdownImageView from '../../module-views/CountdownImageView';
 import ParallaxImageDateView from '../../module-views/ParallaxImageDateView';
 import DressCodeView from '../../module-views/DressCodeView';
 import GiftEnvelopesView from '../../module-views/GiftEnvelopesView';
+import AdultsOnlyNoticeView from '../../module-views/AdultsOnlyNoticeView';
 import ClosingMessageView from '../../module-views/ClosingMessageView';
 import WelcomeMessageView from '../../module-views/WelcomeMessageView';
 import MusicPlayerView from '../../module-views/MusicPlayerView';
@@ -32,6 +33,7 @@ const MODULE_COMPONENTS = {
   parallax_image_date: ParallaxImageDateView,
   dresscode: DressCodeView,
   gift_envelopes: GiftEnvelopesView,
+  adults_only_notice: AdultsOnlyNoticeView,
   closing_message: ClosingMessageView,
   welcome_message: WelcomeMessageView,
   music_player: MusicPlayerView,
